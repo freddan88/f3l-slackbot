400d257b20323e3b9abaf6a1e0a54a55116b7033
Fredrik Leemann <fredrik@leemann.se>
2019-04-16 09:57:23 +0200

    Added logging using winston

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 require('dotenv').config();
+const logger = require ('./logger');
 
 console.log(process.env.NODE_ENV);
+logger.info('This is sent to a combined.log file');
+logger.error('This is sent to a error.log file');
+logger.debug('Console');
