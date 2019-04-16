0d906ee72fba223b2774fc420831f536df3e6749
Fredrik Leemann <fredrik@leemann.se>
2019-04-16 13:45:23 +0200

    ...

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const path = require('path');
+const Botkit = require('botkit');
 
 const logger = require(path.join(__dirname, 'logger'));
 
@@ -12,8 +13,41 @@ logger.debug('Console');
     'NODE_ENV'
 ].forEach(name => {
     if(!process.env[name]){
-        const log = `Ènvironment variable ${name} is missing`;
+        const log = `Environment variable ${name} is missing`;
         logger.error(log);
         throw new Error(log);
     }
 })
+
+const controller = Botkit.slackbot({
+    json_file_store: './db_slackbutton_slash_command/',
+    debug: process.env.NODE_ENV === 'production' ? 'false' : 'true',
+    clientSigningSecret: process.env.CLIENT_SIGNING_SECRET
+});
+
+controller.configureSlackApp({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    clientSigningSecret: process.env.CLIENT_SIGNING_SECRET,
+    scopes: ['commands','bot','chat:write:bot','channels:read','im:reaad','incoming-webhook']
+});
+
+const bot = controller.spawn({
+    token: process.env.BOT_TOKEN,
+    incoming_webhook: {
+        url: 'https://hooks.slack.com/services/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'
+    }
+}).startRTM();
+
+const PORT = process.env.PORT || 3000;
+controller.setupWebserver(PORT, (err, webserver) => {
+    controller.createOauthEndpoints(controller.webserver, 
+        (err, req, res) => {
+            if (err) {
+                logger.error(err);
+                res.status(500).send(`Error: ${err}`);
+            }
+
+            res.send('Connected to Slack');
+        })
+});
