5b3d0d535c56d92df1c260cdc86db9faede30f12
Fredrik Leemann <fredrik@leemann.se>
2019-04-23 09:16:12 +0200

    ...

--- skills/api.js
+++ skills/api.js
@@ -3,50 +3,30 @@ const fetch = require('node-fetch');
 const logger = require('../logger');
 
 module.exports = (controller) => {
-    controller.hears(['Joke', 'Tell me a joke', 'Give me a joke'], 'direct_message,direct_mention', (bot, message) => {
-        
+    controller.hears(['Joke', 'Chuck', 'Tell me a joke', 'Give me a joke'], 'direct_message,direct_mention', (bot, message) => {
         bot.startConversation(message, jokeGenerator);
     });
 
     const jokeGenerator = (res, convo) => {
 
-        convo.say('Chose a category, available categories: Miscellaneous, Programming, Dark, Any');
+        convo.say('Chose a category: dev, movie, food, science, animal, history, music');
         convo.ask('...', async (res, convo) => {
+        convo.say(`Sure. I will search for a joke within ${res.text} category`);
 
-            convo.say(`Sure. I will search for a joke within ${res.text} category`);
+            try{
+                const apiUrl = `https://api.chucknorris.io/jokes/random?category=${res.text}`;
+                const r = await fetch(apiUrl);
+                const joke = await r.json();
 
-            const r = await fetch(`https://sv443.net/jokeapi/category/${res.text}?blacklistFlags=nsfwreligiouspolitical`);
- 
-            const joke = await r.json();
-            console.log(joke);
-            convo.say(joke.setup);
-            convo.next();
-            // try {
-            //     const r = await fetch(`https://sv443.net/jokeapi/category/${res.text}?blacklistFlags=nsfwreligiouspolitical`);
-
-            //     if(r.status !== 200){
-            //         throw new Error(r.statusText);
-            //     }
-            //     const joke = await r.json();
-            //     convo.say(joke);
-            //     convo.next();
-
-            // } catch (err) {
-            //     logger.error(err);
-            //     convo.say('Oops, wrong category. Plearse try again!');
-            //     convo.next();
-
-            // }
+                console.log(joke);
+                convo.say(joke.value);
+                convo.next();
+        
+            }catch (err){
+                logger.error(err);
+                convo.say('Oops, wrong category. Plearse try again!');
+                convo.next();
+            }
         });
     }
-
-
-
-
-
-
-
-
-
-
 }
